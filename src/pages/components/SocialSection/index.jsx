c85c5313ab808c6a6a73e0c175f06ce0dde40917
Gabriel Soares <gabrielsoaresii@outlook.com>
2020-07-01 17:23:34 -0300

    Fixing: Footer media URLs

--- src/pages/components/SocialSection/index.jsx
+++ src/pages/components/SocialSection/index.jsx
@@ -7,10 +7,34 @@ import './styles.css';
 export default function SocialSection(){
   return (
     <div className="Social__Section">
-      <FaLinkedinIn />
-      <FaFacebookF />
-      <FaInstagram />
-      <FaTwitter />
+      <a 
+        href="https://www.linkedin.com/school/universidadecandidomendes/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedinIn />
+      </a>
+      <a 
+        href="https://www.facebook.com/ucamoficial/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaFacebookF />
+      </a>
+      <a 
+        href="https://www.instagram.com/ucam.oficial/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram />
+      </a>
+      <a 
+        href="https://twitter.com/UCAM_oficial"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaTwitter />
+      </a>
     </div>
   )
 }
